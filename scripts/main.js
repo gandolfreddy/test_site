ce672e9955fe823be217d68a2206feb18f1ca70f
FreddyHWWang <freddy.wei0810@gmail.com>
2019-02-07 00:12:12 +0800

    190207

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = "Hello world!";
+// alert("This is an alert");
+
+// document.querySelector("html").onclick = function() {
+//     alert("Ouch! Stop poking me!");
+// }
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/gandalf.jpg')
+        myImage.setAttribute('src', 'images/gandalf_1.jpg')
+    else
+        myImage.setAttribute('src', 'images/gandalf.jpg');
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) 
+    setUserName();
+else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
